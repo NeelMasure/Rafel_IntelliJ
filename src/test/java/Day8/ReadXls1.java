6f57a4cd442fb4d5df29779e68654acafe7abbbe
NeelMasure <masureneel96@gmail.com>
2021-01-27 06:25:43 +0530

    26/01

--- src/test/java/Day8/ReadXls1.java
+++ src/test/java/Day8/ReadXls1.java
@@ -5,11 +5,13 @@ import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Sheet;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ReadXls1 {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         /**
          * xls -- hssf - horrible spreadsheet
@@ -17,24 +19,25 @@ public class ReadXls1 {
          * xlsx --- xssf - Xml spreadsheet format
          */
 
-        String rpath= System.getProperty("user.dir")+ File.separator+"TestData"+File.separator+"TestDataFile.xls";
+        String rpath = System.getProperty("user.dir") + File.separator + "TestData" + File.separator + "TestDataFile.xls";
 
         File file;
-//        FileInputStream fis = new FileInputStream(rpath);
-        HSSFWorkbook wb = new HSSFWorkbook();
-       Sheet sh = wb.getSheet("List");
+        FileInputStream fis = new FileInputStream(rpath);
+        HSSFWorkbook wb = new HSSFWorkbook(fis);
+        Sheet sh = wb.getSheet("List");
 
-            int rowNum = sh.getLastRowNum();
-               System.out.println("Ttal Rows"+rowNum);
+        int rowNum = sh.getLastRowNum();
+        System.out.println("Ttal Rows" + rowNum);
 
-       DataFormatter df = new DataFormatter();
-       String val1 = df.formatCellValue(sh.getRow(1).getCell(0));
-       System.out.println(val1);
+        for (int i = 0; i <= rowNum; i++) {
+            DataFormatter df = new DataFormatter();
+            String name = df.formatCellValue(sh.getRow(i).getCell(0));
+            String mob = df.formatCellValue(sh.getRow(i).getCell(1));
+            System.out.println(name + " ----" + mob);
 
+
+        }
 
     }
-
-
-
 
 }
